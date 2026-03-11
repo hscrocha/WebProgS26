839d58ff2e3de7fe7e1d3eb8336304a0132b122b
Henrique Rocha <hsrocha@loyola.edu>
2026-03-11 12:45:56 -0400

    Lecture 21 - Creating new User (Front and back end) Created a boostrap modal for the New User Created a form for the New User (with option to reuse it for edit user) Added to the back the "/user" post action "/user" post reads info sent by the front end form, and saves it on backend memory.

--- expserver1.js
+++ expserver1.js
@@ -13,8 +13,11 @@ const morgan = require('morgan'); //imports morgan
 const app = express(); //creates a new Express Application
 
 app.use(morgan('dev')); //For better logging, we use morgan
-
 app.use( express.static('public_html') ); // Static page server will use the folder 'public_html'
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
 
 app.get("/user",function(req,res){
     res.status(200); // Ok status
@@ -33,15 +36,36 @@ app.get("/user/:uid",function(req,res){
         }
     }
 
-    if(user != null){
+    if(user != null){ // requested user exists
         res.status(200);
-        res.send(user);
-    } else {
-        res.status(404); 
+        res.send(user); //send the user data
+    } else { // user id does not exists
+        res.status(404);  // error
         res.send({msg:'User with this ID does not exists'});
     }
     res.end();
+});
 
+app.post("/user",function(req,res){
+    let uname = req.body.txt_name; //always red.body.<inputName>
+    let ulogin = req.body.txt_login;
+    let upwd = req.body.txt_password;
+    let uperm = 2; 
+    if (req.body.txt_permission ){
+        uperm = parseInt( req.body.txt_permission );
+    }
+
+    //Generating ID by ourselves, take the last item in the array and adds 1 to their id
+    let uid = lstUsers[ lstUsers.length -1 ]._id +1;
+    //The ID generation above is just for the current memory array example.
+    //DO NOT try to generate ids yourself when using a database.
+
+    let newuser = {_id: uid, name: uname, login: ulogin, permission: uperm}; //creates a user object (like the ones we have on lstUsers array)
+
+    lstUsers.push( newuser ); //adds to the array (manual insertion)
+    //DO NOT do this when using a database
+
+    res.redirect("userpage.html"); //redirects output to this webpage
 });
 
 
